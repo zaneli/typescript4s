def7add255a955bcf532efe3c6dca0cfeb923536
zaneli <shun.otani@gmail.com>
2014-06-15 00:20:51 +0900

    combine lib.d.ts file

--- src/main/scala/com/zaneli/typescript4s/ScriptResources.scala
+++ src/main/scala/com/zaneli/typescript4s/ScriptResources.scala
@@ -1,13 +1,24 @@
 package com.zaneli.typescript4s
 
 import org.apache.commons.io.IOUtils
+import scala.io.Source
 
 private[typescript4s] object ScriptResources {
-  private[typescript4s] val defaultLibNames: Seq[String] = (0 to 15) map (i => f"ts4s.lib${i}%02d.d.ts")
-  private[typescript4s] lazy val defaultLibs: Map[String, String] = (defaultLibNames map { name =>
-    (name -> IOUtils.toString(this.getClass.getResourceAsStream(s"/tsc/${name}")))
-  }).toMap
+
+  private[typescript4s] lazy val defaultLibNames: List[String] = lineNums map (i => s"ts4s.lib${i}.d.ts")
+
+  private[typescript4s] lazy val defaultLibs: Map[String, String] = {
+    val defaultLib: List[String] = Source.fromInputStream(this.getClass.getResourceAsStream("/tsc/lib.d.ts")).getLines.toList
+    require(defaultLib.size == lineNums.last)
+    val libs = (0 :: lineNums).zip(lineNums) map {
+      case (s, e) =>
+        defaultLib.slice(s, e + 1).mkString(System.getProperty("line.separator"))
+    }
+    defaultLibNames.zip(libs).toMap
+  }
 
   private[typescript4s] lazy val tsServices = IOUtils.toString(this.getClass.getResourceAsStream("/tsc/typescriptServices.js"))
   private[typescript4s] lazy val ts4s = IOUtils.toString(this.getClass.getResourceAsStream("/tsc/ts4s.js"))
+
+  private[this] val lineNums = List(974, 1912, 2832, 3761, 3941, 5912, 6703, 7489, 8425, 9340, 10334, 11215, 12203, 13244, 14958)
 }
